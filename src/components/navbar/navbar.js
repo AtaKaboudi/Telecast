deee62df0db0f743830a3ad5834fd90fc9546aaa
AtaKaboudi <ata.kaboudi@gmail.com>
2021-07-06 17:06:59 +0100

    Search

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,16 +1,16 @@
 
 import { useState } from 'react';
 import './navbar.css'
-function NavBar() {
-    function search() {
-
+function NavBar(props) {
+    function search(event) {
+        props.search(event.target[0].value);
     }
     let [searchValue, setSearchValue] = useState("Search");
     return (
         <div className="navBarWrapper">
             <label>TELECT</label>
             <div>
-                <form onSubmit={(e) => { e.preventDefault(); search() }}  >
+                <form onSubmit={(e) => { e.preventDefault(); search(e); }}  >
                     <input value={searchValue} onClick={() => { setSearchValue("") }} onChange={(e) => { setSearchValue(e.target.value) }}></input>
                 </form>
             </div>
--- src/services/youtubeAPI.js
+++ src/services/youtubeAPI.js
@@ -1,6 +1,21 @@
 import axios from 'axios'
 import env from '../env'
 var youtubeAPI = {
+    async search() {
+        let result;
+        console.log(env.OAUTH_TOKEN + '/');
+        return axios({
+            method: 'GET',
+            url: 'https://www.googleapis.com/youtube/v3/search',
+            headers: {
+                'Authorization': `Bearer ${env.OAUTH_TOKEN}`
+            },
+            params: {
+                part: "snippet",
+                maxResults: 12,
+            }
+        })
+    },
     async search(key) {
         let result;
         console.log(env.OAUTH_TOKEN + '/');
@@ -12,6 +27,8 @@ var youtubeAPI = {
             },
             params: {
                 part: "snippet",
+                q: key,
+                maxResults: 12,
             }
         })
     },
